330920f28da7ef3ee881bc99ce20060e0b8969ed
Ioanna Zapalidi <ZapDos7@users.noreply.github.com>
2017-04-19 19:42:32 +0300

    Uploaded anything, palindrome and test

---
+++ palindrome.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int nextPal (int a)
+{
+	
+}
+
+int main()
+{
+	//write the value of the smallest palindrome larger than K
+	int t, x;
+	cin << t;
+	for (int i=0;i<t;i++)
+	{
+		cin << x;
+		nextPal(x);
+	}
+
+	return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,16 +1,11 @@
 #include <iostream>
+
 using namespace std;
 
-int main(/*int argc, char const *argv[]*/) {
-cout << "Hello WORLD MODAFAKAZ!" << endl;
-cout <<"We're like SO GROWN UPS O.M.G. :^ )" << 5 << endl;
-string a="100";
-do {
-cin>>a;
-if(a=="abra")
-  {
-    cout <<"Surprise!!! Thelw kafe! -.-";
-  }
-}while (a!="0");
-  return 0;
+int main()
+{
+	int a, b;
+	cin >> a >> b;
+	cout << a+b;
+	return 0;
 }
